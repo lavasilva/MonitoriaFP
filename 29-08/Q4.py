eb905f19ab80ab5159d5a18af42c2035f29fb320
Lavinia <lmfs@cesar.school>
2025-08-29 19:28:13 -0300

    att

--- 29-08/Q4.py
+++ 29-08/Q4.py
@@ -2,7 +2,7 @@ area = float(input("Digite a área do terreno para a classificação: "))
 
 if 0 < area < 100:
     print("Classificação: Terreno Popular")
-elif area >= 100 or area <= 500:
+elif 100 <= area <= 500: # area <= 100 and area <= 500
     print("Classificação: Terreno Master")
 else: 
     print("Classificação: Terreno VIP")
